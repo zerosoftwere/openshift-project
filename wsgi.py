a26afa6b6b3bd36e9b93b5cab70a840b64cbc5f4
Alhassan Ibrahim <alhassan.ibrahim@flexisaf.net>
2019-12-14 02:34:13 +0100

    Added sleep feature

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from time import sleep
 
 application = Flask(__name__)
 
@@ -10,6 +11,12 @@ def index():
 def greet():
     name = request.args.get('name')
     return jsonify({'message': f'hello {name if name else "there"}'})
+
+@application.route('/api/sleep')
+def destroy():
+    duration = request.args.get('duration')
+    sleep(duration if duration else 30)
+    return jsonify({'message': 'good morning'})
 
 if __name__ == '__main__':
     app.run(debug=True)
